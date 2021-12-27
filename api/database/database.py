edb980f91afade4019feef9d57387b46e189820a
Johnny <aclich.32@gmail.com>
2021-12-27 14:38:25 +0800

    add deploy setting

--- api/database/database.py
+++ api/database/database.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+import os
 from flask_restful import Resource
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-import api.conf.config as conf
 from sqlalchemy.orm import sessionmaker, scoped_session
 from flask import current_app
 from flask import _app_ctx_stack
@@ -13,6 +12,11 @@ from sqlalchemy import create_engine
 # Create sql alchemy database object.
 db = SQLAlchemy()
 
+if os.path.exists(os.path.join('api','conf','config.py')):
+    from api.conf.config import SQLALCHEMY_DATABASE_URI
+else:
+    from api.conf.heroku_env import SQLALCHEMY_DATABASE_URI
+
 class MySQLDBSession(object):
     def __init__(self, app=None):
         self.app = app
@@ -20,7 +24,7 @@ class MySQLDBSession(object):
             self.init_app(app)
 
     def init_app(self, app):
-        engine = create_engine(conf.SQLALCHEMY_DATABASE_URI,
+        engine = create_engine(SQLALCHEMY_DATABASE_URI,
                                pool_size=20,
                                pool_recycle=1800,
                                pool_timeout=30,
